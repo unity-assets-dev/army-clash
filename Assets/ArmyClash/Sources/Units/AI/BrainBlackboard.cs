93a854326e8fd1a352c1d243ed97c1717df516b0
Carhvax <cjjoker@yandex.ru>
2026-01-19 09:26:53 +0200

    - added runtime debugger; - remove aggressive optimisation;

--- Assets/ArmyClash/Sources/Units/AI/BrainBlackboard.cs
+++ Assets/ArmyClash/Sources/Units/AI/BrainBlackboard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
@@ -10,9 +11,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Create BrainBlackboard", fileName = "BrainBlackboard", order = 0), BurstCompile]
 public class BrainBlackboard : ScriptableObject {
     [SerializeField] private bool _usePushBack;
-    
+
     private readonly Dictionary<Type, List<Actor>> _actors = new(2);
-    
+
     public void AddBrain(Actor actor) {
         _actors.TryAdd(actor.GetType(), new List<Actor>(200));
         _actors[actor.GetType()].Add(actor);
@@ -26,37 +27,48 @@ public class BrainBlackboard : ScriptableObject {
 
     public bool TryUsePushBack(Actor actor) {
         if (!_usePushBack) return false;
-        
+
         var chances = actor.HealthPercent > .5f ? 5 : 15;
         return UnityEngine.Random.Range(0, chances) >= 5;
     }
-    
-    public Actor GetNearestAliveTarget(Actor actor) => GetNearestTarget(actor, out var nearestTarget) ? nearestTarget : null;
+
+    public Actor GetNearestAliveTarget(Actor actor) =>
+        GetNearestTarget(actor, out var nearestTarget) ? nearestTarget : null;
 
     private List<Actor> GetEnemyTeam(Actor actor) {
         foreach (var key in _actors.Keys) {
-            if(key != actor.GetType()) return _actors[key];
+            if (key != actor.GetType()) return _actors[key];
         }
+
         return null;
     }
 
     public bool GetNearestTarget(Actor actor, out Actor target) {
-        var neighbors = GetEnemyTeam(actor);
-        
-        var job = GetNearestJob.Create(actor, neighbors).CompleteSchedule(neighbors.Count);
-        
-        target = neighbors[job.Target];
+        var key = _actors.Keys.FirstOrDefault(k => k != actor.GetType());
+        target = null;
+
+        if (key == null) return false;
+        var nearest = float.MaxValue;
+
+        foreach (var neighbor in _actors[key]) {
+            if (neighbor.Dead()) continue;
+
+            var distance = Vector3.Distance(actor.transform.position, neighbor.transform.position);
+            if (distance < nearest || target != null && target.Health > neighbor.Health) {
+                target = neighbor;
+            }
+        }
+
         return target != null;
     }
-    
+
     public void GetSurroundedCount(Actor actor, int range, in HashSet<Actor> surroundingEnemy) {
         surroundingEnemy.Clear();
 
-        var enemyTeam = GetEnemyTeam(actor);
-        var job = GetSurroundingCountJob.Create(actor, range, enemyTeam).CompleteSchedule(enemyTeam.Count);
-
-        foreach (var result in job.Result) {
-            surroundingEnemy.Add(enemyTeam[result]);
+        foreach (var enemy in GetEnemyTeam(actor)) {
+            if (Vector3.Distance(actor.transform.position, enemy.transform.position) <= range) {
+                surroundingEnemy.Add(enemy);
+            }
         }
     }
 }
---
+++ Assets/StompyRobot/SROptions/SROptions.Cheats.cs
@@ -0,0 +1,8 @@
+#if !DISABLE_SRDEBUGGER
+using SRDebugger;
+using SRDebugger.Services;
+#endif
+
+public partial class SROptions {
+    
+}
